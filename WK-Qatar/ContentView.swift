021f7ca8161b7a6df2f3cb8af4d77d0aa54b434c
efrain mouton <efrain.mouton@student.vives.be>
2025-10-20 09:50:25 +0200

    Initial Commit

---
+++ WK-Qatar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WK-Qatar
+//
+//  Created by Efrain Mouton on 20/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WK-Qatar/WK_QatarApp.swift
@@ -0,0 +1,17 @@
+//
+//  WK_QatarApp.swift
+//  WK-Qatar
+//
+//  Created by Efrain Mouton on 20/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct WK_QatarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
